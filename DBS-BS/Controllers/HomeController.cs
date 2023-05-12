9a090d2dc58f563ba76ed7cea3d7c77d46884b0a
Jakub Tesař <73097855+JakubTesar@users.noreply.github.com>
2023-05-12 13:34:34 +0200

    Twitter Done

--- DBS-BS/Controllers/HomeController.cs
+++ DBS-BS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using DBS_BS.FormModels;
 using Microsoft.AspNetCore.Mvc;
 using DBS_BS.Models;
 
@@ -15,13 +16,74 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        _dbContext.Users.Add(new TweetEntity()
+       /* _dbContext.Users.Add(new TweetEntity()
         {
             Content = "Test",
             Author = "a@a.cz"
+        });*/
+       // _dbContext.SaveChanges();
+        
+        return View(_dbContext.Users.ToList());
+    }
+    public IActionResult CreateTweet()
+    {
+        return View(new TweetFormModel());
+    }
+    [HttpPost]
+    public IActionResult CreateTweet(TweetFormModel tweetFormModel)
+    {
+        AddTweet(tweetFormModel);
+        return Redirect("Index");
+    }
+
+    public void AddTweet(TweetFormModel tweetFormModel)
+    {
+        _dbContext.Users.Add(new TweetEntity()
+        {
+            Content = tweetFormModel.Content,
+            Author = tweetFormModel.Author,
+            CreatedAt = DateTime.Now.ToString()
         });
         _dbContext.SaveChanges();
-        
-        return View(_dbContext.Users.Where(i =>i.TweetId == 1).ToList());
+    }
+
+    public IActionResult EditTweet(int id)
+    {
+        return View(_dbContext.Users.Where(i => i.TweetId == id).ToList()[0]);
+    }
+    [HttpPost]
+    public IActionResult EditTweet(TweetEntity tweetEntity)
+    {
+        EditDBTweet(tweetEntity);
+        return Redirect("/Home");
+    }
+
+    public void EditDBTweet(TweetEntity tweetEntity)
+    {
+        if (_dbContext.Users.Where(i => i.TweetId == tweetEntity.TweetId).ToList()[0].TweetId == tweetEntity.TweetId)
+        {
+            var tweet = _dbContext.Users.Where(i => i.TweetId == tweetEntity.TweetId).ToList()[0];
+            tweet.Author = tweetEntity.Author;
+            tweet.Content = tweetEntity.Content;
+            tweet.UpdatedAt = DateTime.Now.ToString();
+        }
+        _dbContext.SaveChanges();
+    }
+
+    public IActionResult LikeTweet(int id)
+    {
+        var tweet = _dbContext.Users.Where(i => i.TweetId == id).ToList()[0];
+        tweet.Likes++;
+        _dbContext.SaveChanges();
+        return Redirect("/Home");
+
+    }
+    public IActionResult DeleteTweet(int id)
+    {
+        var tweet = new TweetEntity() { TweetId = id };
+        _dbContext.Users.Attach(tweet);
+        _dbContext.Users.Remove(tweet);
+        _dbContext.SaveChanges();
+        return Redirect("/");
     }
 }
---
+++ DBS-BS/FormModels/TweetFormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DBS_BS.FormModels;
+
+public class TweetFormModel
+{
+    public string? Content { get; set; }
+    public string? Author { get; set; }
+    public int Likes { get; set; }
+    public string? CreatedAt  { get; set; }
+    public string? UpdatedAt  { get; set; }
+}
